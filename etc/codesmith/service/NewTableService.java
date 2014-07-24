27fb5c3748fc7ba9c050542890c6962a99642271
working <cqiyi@hotmail.com>
2014-07-24 09:10:22 +0800

    初始化工程

---
+++ etc/codesmith/service/NewTableService.java
@@ -0,0 +1,41 @@
+﻿package org.hhwy.rdc.fweb1.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.hhwy.rdc.fweb1.DataVersionConflictException;
+import org.hhwy.rdc.fweb1.domain.NewTable;
+import org.hhwy.rdc.fweb1.mapper.NewTableMapper;
+
+@Service
+public class NewTableService {
+    
+	@Autowired
+    private NewTableMapper mapper;
+
+	  public void insert(NewTable newTable){
+        mapper.insert(newTable);
+      }
+	  
+	  public void update(NewTable newTable){
+          if(mapper.getVersion(newTable.getId()) != newTable.getVersion()){
+            throw new DataVersionConflictException("Updating new_table." + newTable.getId() );
+          }
+          
+        mapper.update(newTable);
+      
+      }
+	  
+	  public void delete(String id){
+        mapper.delete(id);
+      }
+	  
+	  public NewTable get(String id){
+        return mapper.get(id);
+      }
+	  
+	  public List<NewTable> getAll(){
+        return mapper.getAll();
+      }
+}
+
---
+++ src/main/java/org/hhwy/fweb1/service/FieldTypesService.java
@@ -0,0 +1,42 @@
+﻿package org.hhwy.fweb1.service;
+
+import java.util.List;
+
+import org.hhwy.fweb1.DataVersionConflictException;
+import org.hhwy.fweb1.domain.FieldTypes;
+import org.hhwy.fweb1.mapper.FieldTypesMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FieldTypesService {
+    
+	@Autowired
+    private FieldTypesMapper mapper;
+
+	  public void insert(FieldTypes fieldTypes){
+        mapper.insert(fieldTypes);
+      }
+	  
+	  public void update(FieldTypes fieldTypes){
+          if(mapper.getVersion(fieldTypes.getId()) != fieldTypes.getVersion()){
+            throw new DataVersionConflictException("Updating t_field_types." + fieldTypes.getId() );
+          }
+          
+        mapper.update(fieldTypes);
+      
+      }
+	  
+	  public void delete(String id){
+        mapper.delete(id);
+      }
+	  
+	  public FieldTypes get(String id){
+        return mapper.get(id);
+      }
+	  
+	  public List<FieldTypes> getAll(){
+        return mapper.getAll();
+      }
+}
+
